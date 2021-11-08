60beea170914e33f3970285bdc9ea3759be68b47
tarnilok <tarnilok@gmail.com>
2021-11-08 22:39:04 +0300

    todoListUpdate added

--- apiTodo/urls.py
+++ apiTodo/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home, todoList, todoListCreate
+from .views import home, todoList, todoListCreate,todo_List, todoListUpdate
 
 urlpatterns = [
     path('', home),
     path('todoList/', todoList),
     path('todoListCreate/', todoListCreate),
+    path('todo_List/', todo_List),
+    path('todoListUpdate/<int:pk>', todoListUpdate),
 ]
--- apiTodo/views.py
+++ apiTodo/views.py
@@ -4,6 +4,7 @@ from .serializers import TodoSerializer
 from .models import Todo
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 def home(request):
     return HttpResponse('<center><h1 style="background-color:powderblue;">Welcome to ApiTodo</h1></center>')
@@ -23,6 +24,28 @@ def todoListCreate(request):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+@api_view(['POST', 'GET'])
+def todo_List(request):
+    if request.method == 'GET':
+        queryset = Todo.objects.all()
+        serializer = TodoSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['PUT'])
+def todoListUpdate(request, pk):
+    queryset = Todo.objects.get(id=pk)
+    serializer = TodoSerializer(instance=queryset, data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+        
 
 
 
